1c4292ebad4d8d71cd3f9d1538453aac539c0d60
dwane richards <dwanerich@gmail.com>
2020-03-01 21:25:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,9 +4,47 @@ class Dog
   attr_reader :id
   
   def initialize(id=nil, name:, breed:)
-    @id = id
+    # @id = id
     @name = name
     @breed = breed
   end
+  
+  def self.create_table(id, name, breed)
+    
+    
+  end
+  
+  def self.drop_table
+    
+  end
+  
+  def save
+    
+  end
+  
+  def self.create
+    
+  end
+  
+  def self.new_from_db
+    
+  end
+  
+  def self.find_by_id
+    
+  end
+  
+  def self.find_or_create_by
+    
+  end
+  
+  def self.find_by_name
+    
+  end
+  
+  def update
+    
+  end
+  
   
 end
