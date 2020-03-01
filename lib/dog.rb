5ca9693d86622c550a048f41cc8944ba82893792
dwane richards <dwanerich@gmail.com>
2020-03-01 21:28:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,8 +3,8 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
   
-  def initialize(id=nil, name:, breed:)
-    @id = nil
+  def initialize(name:, breed:)
+    @id = id
     @name = name
     @breed = breed
   end
